aec9ecdb378413267543eb6ee67346054158e8a1
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-30 14:18:16 +0530

    Update Footer and PDP

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -26,8 +26,8 @@ const Footer: FC<Props> = ({ config }) => {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="border-t border-gray-200 py-20">
+      <div className="sm:w-4/5 w-full mx-auto sm:px-0 px-4">
+        <div className="py-20">
           <div className="grid grid-cols-1 md:grid-cols-12 md:grid-flow-col md:gap-x-8 md:gap-y-16 md:auto-rows-min">
             {/* Image section */}
             <div className="col-span-1 md:col-span-2 lg:row-start-1 lg:col-start-1">
@@ -90,12 +90,13 @@ const Footer: FC<Props> = ({ config }) => {
                   type="text"
                   autoComplete="email"
                   required
-                  className="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-gray-900 placeholder-gray-500 focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                  placeholder='Enter Email'
+                  className="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-sm shadow-sm py-2 px-4 text-gray-900 placeholder-gray-400 focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500 hover:border-black"
                 />
                 <div className="ml-4 flex-shrink-0">
                   <button
                     type="submit"
-                    className="w-full bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="w-full bg-black border border-transparent rounded-sm shadow-sm py-2 px-4 flex items-center justify-center font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:bg-pink"
                   >
                     {BTN_SIGN_UP}
                   </button>
--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -400,10 +400,9 @@ export default function ProductView({
       <main className="sm:pt-8">
         <div className="lg:max-w-none">
           {/* Product */}
-          <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:items-start">
-             
+          <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:items-start">             
             <div className="flex flex-col-reverse lg:col-span-7">
-              <div className='grid grid-cols-2 gap-x-8 gap-y-6'>
+              <div className='grid grid-cols-2 gap-x-2 gap-y-1'>
                 {content?.map((image: any, idx) => (
                   <div key={idx}>
                     <span className="sr-only">{image.name}</span>
@@ -498,6 +497,14 @@ export default function ProductView({
                       action={buttonConfig.action}
                       buttonType={buttonConfig.type || 'cart'}
                     />
+                    {isEngravingAvailable && (
+                    <button
+                      className="max-w-xs flex-1 ml-2 bg-gray-600 border border-transparent rounded-sm uppercase py-3 px-8 flex items-center justify-center font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500 sm:w-full"
+                      onClick={() => showEngravingModal(true)}
+                    >
+                      <span className="font-bold">{GENERAL_ENGRAVING}</span>
+                    </button>
+                  )}
                     <button
                       type="button"
                       onClick={() => {
@@ -505,7 +512,7 @@ export default function ProductView({
                           handleWishList()
                         }
                       }}
-                      className="ml-4 py-3 px-4 rounded-sm bg-white border border-gray-300 flex items-center justify-center text-gray-500 hover:bg-red-50 hover:text-pink px-20 hover:border-pink"
+                      className="ml-2 py-3 px-4 rounded-sm bg-white border border-gray-300 flex items-center justify-center text-gray-500 hover:bg-red-50 hover:text-pink sm:px-10 hover:border-pink"
                     >
                     {isInWishList ? (
                       <span>{ALERT_SUCCESS_WISHLIST_MESSAGE}</span>
@@ -514,15 +521,7 @@ export default function ProductView({
                     )}
                       <span className="sr-only">{BTN_ADD_TO_FAVORITES}</span>
                     </button>
-                  </div>
-                  {isEngravingAvailable && (
-                    <button
-                      className="max-w-xs flex-1 mt-5 bg-gray-400 border border-transparent rounded-sm uppercase py-3 px-8 flex items-center justify-center font-medium text-white hover:bg-pink focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500 sm:w-full"
-                      onClick={() => showEngravingModal(true)}
-                    >
-                      <span className="font-bold">{GENERAL_ENGRAVING}</span>
-                    </button>
-                  )}
+                  </div>                  
                 </>
               ) : null}
             <section
